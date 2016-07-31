f4eeb6c7a8cc476c253f3f6e4d7320be8645006a
Matthaz <tnmmatthewburgess@gmail.com>
2016-07-31 20:22:49 +1000

    v1.24

--- Spammy/Cogs/assignment.py
+++ Spammy/Cogs/assignment.py
@@ -6,12 +6,14 @@ class Assignment():
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def fetch_image(self, identifier):
-        async with aiohttp.ClientSession() as session:
-            async with session.get('http://imgur.com/gallery/{}'.format(identifier)) as response:
-                print(response.status)
-                await self.bot.say(await response.text())
+    @commands.command(pass_context=True)
+    async def upimg2(self, ctx, url: str):
+        with aiohttp.ClientSession() as session:  # Opens async HTTP session
+            async with session.get(url) as response:  # Sends request, stores response.
+                image = await response.content.read()  # Grabs image data from "stream"
+                with open("unknown.jpg", 'wb') as dl_image:  # opens empty image send_file
+                    dl_image.write(image)  # Writes image data into image file, then closes it
+                await self.bot.send_file(ctx.message.channel, "unknown.jpg")  # uploads image
 
 
 def setup(bot):
